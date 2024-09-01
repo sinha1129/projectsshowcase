64e7d5a8d850dd27491084e1d9e701132eaae883
sinha1129 <sinhabhukya.2003@gamil.com>
2024-09-01 10:59:54 +0000

    Projects Show Case

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import {HeaderContainer, HeaderImage} from './styledComponents'
+
+const Header = () => (
+  <HeaderContainer>
+    <HeaderImage
+      src="https://assets.ccbp.in/frontend/react-js/projects-showcase/website-logo-img.png"
+      alt="webiste logo"
+    />
+  </HeaderContainer>
+)
+export default Header
